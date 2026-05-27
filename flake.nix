d103d017874fe49e576ecebf866ab9282017c8ee
Simon Scatton <sscatton@nvidia.com>
2026-05-27 14:47:00 +0200

    feat(build): add simple nix flake with formatter for nix code

---
+++ flake.nix
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+{
+  description = "OpenShell development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ ];
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+      }
+    );
+}
