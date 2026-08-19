df0663716ac48d013126dede0c1d39a9ab625e5e
politerealism <burdcat17@gmail.com>
2026-08-19 11:45:58 -0400

    fix(sandbox): remove unnecessary std::pin qualification
    Signed-off-by: politerealism <burdcat17@gmail.com>

--- crates/openshell-sandbox/src/lib.rs
+++ crates/openshell-sandbox/src/lib.rs
@@ -3001,7 +3001,7 @@ type MiddlewareConnector = Arc<
     dyn Fn(
             Vec<openshell_core::proto::SupervisorMiddlewareService>,
             MiddlewareAuthentication,
-        ) -> std::pin::Pin<
+        ) -> Pin<
             Box<
                 dyn std::future::Future<
                         Output = Result<openshell_supervisor_middleware::MiddlewareRegistry>,
