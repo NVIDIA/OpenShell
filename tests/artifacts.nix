0c0a0e7a8ec1262d06cc975f9608bb489e2e3da3
Simon Scatton <sscatton@nvidia.com>
2026-09-11 15:12:18 +0200

    test(tmachine): improve artifact builds and diagnostics
    Signed-off-by: Simon Scatton <sscatton@nvidia.com>

--- tests/artifacts.nix
+++ tests/artifacts.nix
@@ -16,39 +16,98 @@ let
     builtins.attrValues toolchains
   );
 in
-pkgs.writeShellApplication {
-  name = "tmachine-artifacts";
-  runtimeInputs = [
-    pkgs.docker-client
-    pkgs.git
-    rustToolchain
-  ];
-  runtimeEnv = toolchainEnv;
-  text = ''
-    root=$(git rev-parse --show-toplevel)
-    cd "$root"
+rec {
+  binaries = pkgs.writeShellApplication {
+    name = "build-artifacts-binaries";
+    runtimeInputs = [
+      pkgs.git
+      rustToolchain
+    ];
+    runtimeEnv = toolchainEnv;
+    text = ''
+      root=$(git rev-parse --show-toplevel)
+      cd "$root"
 
-    cargo build --target ${muslToolchain.target} \
-      -p openshell-cli \
-      -p openshell-conformance-cli \
-      -p openshell-sandbox
+      cargo build --target ${muslToolchain.target} \
+        -p openshell-cli \
+        -p openshell-conformance-cli \
+        -p openshell-sandbox
 
-    cargo build --target ${gnuToolchain.target} \
-      -p openshell-gateway
+      cargo build --target ${gnuToolchain.target} \
+        -p openshell-gateway
+    '';
+  };
 
-    install -D -m 0755 \
-      target/${muslToolchain.target}/debug/openshell-sandbox \
-      deploy/docker/.build/prebuilt-binaries/${dockerArch}/openshell-sandbox
+  images = pkgs.writeShellApplication {
+    name = "build-artifacts-images";
+    runtimeInputs = [
+      pkgs.docker-client
+      pkgs.git
+    ];
+    text = ''
+      root=$(git rev-parse --show-toplevel)
+      cd "$root"
 
-    docker build \
-      --platform linux/${dockerArch} \
-      --file deploy/docker/Dockerfile.supervisor \
-      --target supervisor \
-      --tag openshell/supervisor:tmachine \
-      .
+      install -D -m 0755 \
+        target/${gnuToolchain.target}/debug/openshell-gateway \
+        deploy/docker/.build/prebuilt-binaries/${dockerArch}/openshell-gateway
 
-    docker save \
-      --output target/openshell-supervisor-tmachine.tar \
-      openshell/supervisor:tmachine
-  '';
+      install -D -m 0755 \
+        target/${muslToolchain.target}/debug/openshell-sandbox \
+        deploy/docker/.build/prebuilt-binaries/${dockerArch}/openshell-sandbox
+
+      docker build \
+        --platform linux/${dockerArch} \
+        --file deploy/docker/Dockerfile.gateway \
+        --target gateway \
+        --tag openshell/gateway:tmachine \
+        .
+
+      docker build \
+        --platform linux/${dockerArch} \
+        --file deploy/docker/Dockerfile.supervisor \
+        --target supervisor \
+        --tag openshell/supervisor:tmachine \
+        .
+
+      mkdir -p artifacts/images
+
+      docker save \
+        --output artifacts/images/openshell-gateway-tmachine.tar \
+        openshell/gateway:tmachine
+
+      docker save \
+        --output artifacts/images/openshell-supervisor-tmachine.tar \
+        openshell/supervisor:tmachine
+    '';
+  };
+
+  helm = pkgs.writeShellApplication {
+    name = "build-artifacts-helm";
+    runtimeInputs = [
+      pkgs.git
+      pkgs.kubernetes-helm
+    ];
+    text = ''
+      root=$(git rev-parse --show-toplevel)
+      cd "$root"
+
+      mkdir -p artifacts/helm
+      helm package deploy/helm/openshell --destination artifacts/helm
+    '';
+  };
+
+  all = pkgs.writeShellApplication {
+    name = "build-artifacts";
+    runtimeInputs = [
+      binaries
+      images
+      helm
+    ];
+    text = ''
+      build-artifacts-binaries
+      build-artifacts-images
+      build-artifacts-helm
+    '';
+  };
 }
