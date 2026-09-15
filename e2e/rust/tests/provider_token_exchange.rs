0cfc777ede080fea1995b3fd599423909b30b6a5
Varsha Prasad Narsing <varshaprasad96@gmail.com>
2026-09-14 19:27:08 -0700

    refactor(crypto): centralize AWS-LC behind backend interface
    Signed-off-by: Varsha Prasad Narsing <varshaprasad96@gmail.com>

--- e2e/rust/tests/provider_token_exchange.rs
+++ e2e/rust/tests/provider_token_exchange.rs
@@ -129,7 +129,7 @@ impl SpiffeWorkloadApi {
             "iat": now,
             "exp": now + 3600,
         });
-        jsonwebtoken::encode(&header, &claims, &self.encoding_key)
+        openshell_crypto::jwt::encode(&header, &claims, &self.encoding_key)
             .map_err(|err| Status::internal(format!("sign JWT-SVID: {err}")))
     }
 }
